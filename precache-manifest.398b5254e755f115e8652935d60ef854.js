eb3d2aaf0761e6bb3cf1fbc2b4bd639d2e6424a8
Pratikkadam00 <pratikadam00@gmail.com>
2021-07-10 13:13:56 +0530

    Updates

---
+++ precache-manifest.398b5254e755f115e8652935d60ef854.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820f9dbc787a26a0644c54d7b4089115",
+    "url": "/Covid19-tracker/index.html"
+  },
+  {
+    "revision": "04f3a0959ab7671cc978",
+    "url": "/Covid19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "963ed3f73f4932a6326d",
+    "url": "/Covid19-tracker/static/css/main.b8978b6d.chunk.css"
+  },
+  {
+    "revision": "04f3a0959ab7671cc978",
+    "url": "/Covid19-tracker/static/js/2.a860358b.chunk.js"
+  },
+  {
+    "revision": "9922ea9069e02abaf7aeceb5abadc3c8",
+    "url": "/Covid19-tracker/static/js/2.a860358b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "963ed3f73f4932a6326d",
+    "url": "/Covid19-tracker/static/js/main.373d954f.chunk.js"
+  },
+  {
+    "revision": "cac869e8509a012a59e4",
+    "url": "/Covid19-tracker/static/js/runtime-main.b37aa5f6.js"
+  }
+]);
